83385c6352157380b3c3497b73b258f03816f869
yourname <29952469+rwStarnight@users.noreply.github.com>
2018-04-03 16:13:52 -0400

    add Touchable

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
-import { getMetricMetaInfo} from '../utils/helpers';
+import { View, TouchableOpacity, Text } from 'react-native';
+import { getMetricMetaInfo, timeToString } from '../utils/helpers';
 import Slider from './Slider';
 import Steppers from './Steppers';
 import DateHeader from './DateHeader';
+
+function SubmitBtn({ onPress }) {
+    return (
+        <TouchableOpacity
+            onPress={onPress}>
+            <Text>SUBMIT</Text>
+        </TouchableOpacity>
+    )
+}
 
 export default class AddEntry extends Component {
     state = {
@@ -40,6 +49,27 @@ export default class AddEntry extends Component {
             [metric]: value,
         }))
     };
+    submit = () => {
+      const key = timeToString();
+      const entry = this.state;
+
+      // Update Redux
+        
+      this.setState(() => ({
+          run: 0,
+          bike: 0,
+          swim: 0,
+          sleep: 0,
+          eat: 0,
+      }))
+        
+      // Navigate to home
+
+      // Save to DB
+
+      // Clear local notification
+    };
+
     render() {
         const metaInfo = getMetricMetaInfo();
 
@@ -68,6 +98,7 @@ export default class AddEntry extends Component {
                         </View>
                     )
                 })}
+                <SubmitBtn onPress={this.submit}/>
             </View>
         )
     }
