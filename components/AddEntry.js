82eded4d3bca3c11b888591e3b343160d84a08e7
yourname <29952469+rwStarnight@users.noreply.github.com>
2018-04-03 18:08:47 -0400

    create api

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -6,6 +6,7 @@ import mySteppers from './mySteppers';
 import DateHeader from './DateHeader';
 import { Ionicons } from '@expo/vector-icons';
 import TextButton from './TextButton';
+import { submitEntry, removeEntry } from "../utils/api";
 
 function SubmitBtn({ onPress }) {
     return (
@@ -67,18 +68,18 @@ export default class AddEntry extends Component {
         
       // Navigate to home
 
-      // Save to DB
+      submitEntry({ key, entry })
 
       // Clear local notification
     };
     reset = () => {
-        const key = timeToString()
+        const key = timeToString();
 
         // Update Redux
 
         // Navigate to home
 
-        // Save to DB
+        removeEntry(key)
     };
 
     render() {
